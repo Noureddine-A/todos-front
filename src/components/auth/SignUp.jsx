ff071712432be847708a9016e97d30d95f5c6b5a
Noureddine-A <anoxybiosis@hotmail.com>
2024-09-12 18:50:54 +0200

    Added sign up user input validation

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -10,7 +10,12 @@ const SignUp = () => {
 
   return (
     <div className="todo__sign-up-container">
-      <AuthForm title="Sign Up" action="signup" error={actionData?.isError} errorMsg={actionData?.message}/>
+      <AuthForm
+        title="Sign Up"
+        action="signup"
+        error={actionData?.isError}
+        errorMsg={actionData?.message}
+      />
     </div>
   );
 };
@@ -22,6 +27,12 @@ export async function action({ request, params }) {
 
   const username = data.get("username");
   const password = data.get("password");
+
+  const checkedInput = validateInput(username, password);
+
+  if (checkedInput.isError === true) {
+    return { isError: true, message: checkedInput.message };
+  }
 
   const response = await fetch("http://localhost:8080/auth/signup", {
     method: "POST",
@@ -36,13 +47,42 @@ export async function action({ request, params }) {
 
   const resData = await response.json();
 
-  if(response.status !== 200) {
-    return {isError: true, message: resData.error}
+  if (response.status !== 200) {
+    return { isError: true, message: resData.error };
   }
 
   if (!response.ok) {
     return { isError: true, message: "Didn't work" };
   }
 
-  return redirect('/auth/login');
+  return redirect("/auth/login");
+}
+
+export function validateInput(username, password) {
+  let error = {};
+
+  if (password.trim().length < 5 && username.trim().length < 5) {
+    error = {
+      isError: true,
+      message:
+        "Both the password and username needs to be at least 6 characters long."
+    };
+  } else if (password.trim().length < 5) {
+    error = {
+      isError: true,
+      message:
+        "The password needs to be at least 6 characters to be deemed valid.",
+    }
+  } else if( username.trim().length < 5) {
+    error = {
+      isError: true,
+      message: "The username needs to be at least 6 characters to be deemed valid."
+    }
+  } else {
+    error = {
+      isError: false
+    }
+  }
+
+  return error;
 }
--- src/components/auth/util/AuthForm.jsx
+++ src/components/auth/util/AuthForm.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React from "react";
 
 import { Form } from "react-router-dom";
 
-import './AuthForm.css';
+import "./AuthForm.css";
 
-const AuthForm = ({title, error, errorMsg}) => {
+const AuthForm = ({ title, error, errorMsg }) => {
+  let usernameInput = "todo__form-input-container-user-input";
+  let passwordInput = "todo__form-input-container-password-input";
+
+  if (errorMsg?.includes("password") && errorMsg?.includes("username")) {
+    usernameInput = "todo__form-input-container-input-username-error";
+    passwordInput = "todo__form-input-container-input-password-error";
+  } else if (errorMsg?.includes("username")) {
+    usernameInput = "todo__form-input-container-input-username-error";
+  } else if (errorMsg?.includes("password")) {
+    passwordInput = "todo__form-input-container-input-password-error";
+  }
+
   return (
     <Form method="post" className="todo__form-container">
-        <h1>{title}</h1>
-        {error && <p>{errorMsg}</p>}
-        <div className="todo__form-input-wrapper">
-          <div className="todo__form-input-container">
-            <input type="text" name="username" placeholder="Username" />
-            <input type="password" name="password" placeholder="Password" />
-            <div className="todo__form-button-container">
-                <button>{title}</button>
-            </div>
+      <h1>{title}</h1>
+      {error && <p>{errorMsg}</p>}
+      <div className="todo__form-input-wrapper">
+        <div className="todo__form-input-container">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            className={usernameInput}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className={passwordInput}
+          />
+          <div className="todo__form-button-container">
+            <button>{title}</button>
           </div>
         </div>
-      </Form>
-  )
-}
+      </div>
+    </Form>
+  );
+};
 
-export default AuthForm
+export default AuthForm;
